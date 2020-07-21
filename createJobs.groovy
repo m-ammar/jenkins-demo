0ae0ee60c557d63aeeea798e4e2abd8bfd70b3b9
Mohammed Ammar <m.ammar@lavego.de>
2020-07-21 15:17:06 +0200

    Create a new jenkins job

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,17 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/m-ammar/spring-boot-api-example.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
